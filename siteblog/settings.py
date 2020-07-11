d14f82a816770bb98c456e26fbc1ce9a9b992584
frankyjohn <frankyjohn2014@gmail.com>
2020-07-12 01:13:48 +0300

    tes555

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/img')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/img')
 
 
 
@@ -133,8 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'siteblog/static')
 ]
-
-
 
 INTERNAL_IPS = [
     '*',
