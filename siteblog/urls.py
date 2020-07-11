54db387eae6156ab472c1dc8477765e5b5ab7f7e
frankyjohn <frankyjohn2014@gmail.com>
2020-07-12 01:18:56 +0300

    te666

--- siteblog/urls.py
+++ siteblog/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('',include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
